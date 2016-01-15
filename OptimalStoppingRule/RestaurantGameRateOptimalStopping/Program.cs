4f463c6fa872df6f2ae4752a3883046cc0cb89ba
cohenguy1 <cohenguy1@gmail.com>
2016-01-15 10:19:14 +0200

    added optimal stopping projects

---
+++ OptimalStoppingRule/RestaurantGameRateOptimalStopping/Program.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantGameOptimalStopping
+{
+    class Program
+    {
+        public const int Samples = 10;
+
+        public const int NumOfRanks = 20;
+
+        public static Dictionary<int, double> ChosenRankProbabilities = new Dictionary<int, double>()
+        {
+            {1, 0.316205685},
+            {2, 0.238760195},
+            {3, 0.16912668 },
+            {4, 0.110335625 },
+            {5, 0.067916575 },
+            {6, 0.039654305 },
+            {7, 0.02307051 },
+            {8, 0.012919295 },
+            {9, 0.00718162 },
+            {10, 0.00456783 },
+            {11, 0.002691555 },
+            {12, 0.00114881 },
+            {13, 0.000978745 },
+            {14, 0.000875835 },
+            {15, 0.000689325 },
+            {16, 0.000788975 },
+            {17, 0.000698785 },
+            {18, 0.000778985 },
+            {19, 0.00083386 },
+            {20, 0.000776805 }
+        };
+
+        public static int[] stoppingValues = new int[Samples];
+
+        public static void Main(string[] args)
+        {
+            var expectation = GetExpectation(ChosenRankProbabilities);
+
+            stoppingValues[Samples - 1] = NumOfRanks;
+
+            for (int i = Samples - 2; i >= 0; i--)
+            {
+                var stoppingValue = GetProbabilityThatRankLowerOrEqualsThan(stoppingValues[i + 1]) * GetExpectationOrRankLowerOrEquals(stoppingValues[i + 1])
+                                    + GetProbabilityThatRankGreater(stoppingValues[i + 1]) * stoppingValues[i + 1];
+                stoppingValues[i] = stoppingValue > 1 ? (int)stoppingValue : 1;
+            }
+
+            for (int i = Samples - 1; i >= 0; i--)
+            {
+                Console.WriteLine((i + 1) + " Stopping Value: " + stoppingValues[i]);
+            }
+
+            Console.WriteLine("******");
+
+            
+            Console.ReadLine();
+        }
+
+        private static double GetProbabilityThatRankGreater(int rank)
+        {
+            double accumulatedProbability = 0;
+
+            for (int i = rank + 1; i <= NumOfRanks; i++)
+            {
+                accumulatedProbability += ChosenRankProbabilities[i];
+            }
+
+            return accumulatedProbability;
+        }
+
+        private static double GetExpectationOrRankLowerOrEquals(int rank)
+        {
+            double expectation = 0;
+
+            for (int i = 1; i <= rank; i++)
+            {
+                expectation += i * ChosenRankProbabilities[i];
+            }
+
+            return expectation;
+        }
+
+        private static double GetExpectation(Dictionary<int, double> chosenRankProbabilities)
+        {
+            double expectation = 0;
+
+            for (int i = 1; i <= NumOfRanks; i++)
+            {
+                expectation += i * chosenRankProbabilities[i];
+            }
+
+            return expectation;
+        }
+
+        private static double GetProbabilityThatRankLowerOrEqualsThan(int rank)
+        {
+            double accumulatedProbability = 0;
+
+            for (int i = 1; i <= rank; i++)
+            {
+                accumulatedProbability += ChosenRankProbabilities[i];
+            }
+
+            return accumulatedProbability;
+        }
+    }
+}
---
+++ OptimalStoppingRule/UnifromDistributionStoppingRule/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnifromDistributionStoppingRule
+{
+    class Program
+    {
+        public const int Samples = 20;
+
+        public static double[] stoppingValues = new double[Samples];
+
+        public static double[] values = new double[Samples];
+
+        public static void Main(string[] args)
+        {
+            stoppingValues[Samples - 1] = 0;
+
+            for (int i = Samples - 2; i >= 0; i--)
+            {
+                stoppingValues[i] = 50 + 1 / (double)200 * (double)Math.Pow(stoppingValues[i + 1], 2);
+            }
+
+            for (int i = Samples - 1; i >= 0; i--)
+            {
+                Console.WriteLine((i + 1) + " Stopping Value: " + stoppingValues[i]);
+            }
+
+            Console.WriteLine("******");
+
+            Random r = new Random();
+
+            for (int i = 0; i < Samples; i++)
+            {
+                values[i] = r.NextDouble() * (double)100;
+
+                if (values[i] > stoppingValues[i])
+                {
+                    Console.WriteLine("Stopped at: " + (i + 1) + " Value: " + values[i]);
+                    break;
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
